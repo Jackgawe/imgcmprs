6b1383087aba9810cd8be75e17a6b9d2f4a89386
Eyad <108591924+Jackgawe@users.noreply.github.com>
2025-10-11 13:02:39 +0300

    .

--- setup.py
+++ setup.py
@@ -1,10 +1,15 @@
 from setuptools import setup, find_packages
 
+with open("README.md", "r", encoding="utf-8") as fh:
+    long_description = fh.read()
+
 setup(
     name='imgcmprs',
     version='0.1.0',
-    description='A CLI tool for compressing images (JPEG, PNG)',
-    author='Your Name',
+    description='Fast, safe CLI to compress JPEG & PNG, lossless/lossy, batch or single images.',
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    author='Eyad Mohammed',
     packages=find_packages(),
     install_requires=[
         'Pillow'
