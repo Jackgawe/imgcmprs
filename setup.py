2754042b4d0934aec3788c464fb6ca59e1ae11b7
Eyad <108591924+Jackgawe@users.noreply.github.com>
2025-10-11 17:22:30 +0300

    ..

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r", encoding="utf-8") as fh:
 
 setup(
     name='imgcmprs',
-    version='0.1.0',
+    version='0.1.1',
     description='Fast, safe CLI to compress JPEG & PNG, lossless/lossy, batch or single images.',
     long_description=long_description,
     long_description_content_type="text/markdown",
